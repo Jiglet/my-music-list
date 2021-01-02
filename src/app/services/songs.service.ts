e9f9a38f654b12bee26875faa36b6488b0323cc0
Jim Tang <jimruitang@gmail.com>
2021-01-02 16:19:21 -0500

    Changed environment imports. Build for prod

--- src/app/services/songs.service.ts
+++ src/app/services/songs.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Router, UrlSegment } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { environment } from '../environments/environment';
+import { environment } from '../../environments/environment';
 import { User } from '../models';
 
 @Injectable({
