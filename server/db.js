c41879f931efff866f7ddc93209867f2dc61a17c
Jim Tang <jimruitang@gmail.com>
2021-01-02 17:00:34 -0500

    Log databaseURL

--- server/db.js
+++ server/db.js
@@ -4,6 +4,7 @@ const { Pool } = require("pg");
 
 const isProduction = process.env.NODE_ENV == 'production';
 console.log('isProduction: '+isProduction)
+console.log('process.env.DATABASE_URL: '+process.env.DATABASE_URL)
 
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
