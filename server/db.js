c3703ecb138960004f88bf109ae754ebac31e668
Jim Tang <jimruitang@gmail.com>
2021-01-02 17:16:28 -0500

    d

--- server/db.js
+++ server/db.js
@@ -6,8 +6,9 @@ const isProduction = process.env.NODE_ENV == 'production';
 console.log('isProduction: '+isProduction)
 console.log('process.env.DATABASE_URL: '+process.env.DATABASE_URL)
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+console.log('connectionString: '+isProduction ? process.env.DATABASE_URL : connectionString)
 const pool = new Pool({
     connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
     ssl: isProduction
