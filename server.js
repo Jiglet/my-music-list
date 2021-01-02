54a2113bed178342102eb5436cfe224e09e79908
Jim Tang <jimruitang@gmail.com>
2021-01-02 10:52:17 -0500

    Changes for deploy

--- server.js
+++ server.js
@@ -5,10 +5,28 @@ const path = require('path');
 
 app.use(express.static(__dirname + '/dist/my-song-list'));
 
-app.listen(process.env.PORT || 8080);
-
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/my-song-list/index.html'));
 });
 
-console.log('Console listening!');
+const router = express.Router();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const errorHandler = require('./server/middleware/error-handler');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+// API ROUTES
+app.use('/users', require('./server/users/user.controller'));
+app.use('/spotify', require('./server/spotify/spotify.controller'));
+app.use('/track', require('./server/spotify/spotify.controller'));
+app.use('/reviews', require('./server/reviews/reviews.controller'));
+
+// global error handler
+app.use(errorHandler);
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`);
+});
