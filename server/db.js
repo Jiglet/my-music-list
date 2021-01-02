4fe77bef1cf6e3236dcc3d62417650bb6e9e5f2b
Jim Tang <jimruitang@gmail.com>
2021-01-02 18:03:42 -0500

    Removed cl

--- server/db.js
+++ server/db.js
@@ -6,7 +6,6 @@ const isProduction = process.env.NODE_ENV == 'production';
 console.log('isProduction: '+isProduction)
 
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
-console.log('connectionString: '+isProduction ? process.env.DATABASE_URL : connectionString)
 const pool = new Pool({
     connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
     ssl: isProduction
